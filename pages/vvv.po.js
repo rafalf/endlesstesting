9c7e6574be8394d2046d8f315a7642e6c1ba59bd
rafalfusik <rfusik@ammado.com>
2016-12-17 10:47:43 +0000

    finishing test vvv

--- pages/vvv.po.js
+++ pages/vvv.po.js
@@ -8,18 +8,48 @@ var vvvPage = function (){
         return $$('.i-fvv-header-body__login-value');
     };
 
-    // Ir a Panel12 VVV Online button
+    this.getPencilButton = function(){
+        return $('.i-fvv-header-body__login-action')
+    };
+
+    // in:  Panel12 VVV Online
+    this.getOnlineButtons = function() {
+        return $$('.i-fvv-header-body__features [title="clickToGoToPanel12VVVOnline"]');
+    };
+
+    // in any service
     this.getAllOnlineButtons = function() {
         return $$('[title="clickToGoToPanel12VVVOnline"]');
     };
 
-    // Panel12 VVV Online
+    // get all service z3 online service containers
+    // in Con Panel12 VVV Online puedes disfrutar de...
     this.getAllOnlineZ3 = function() {
         return $$('[package-item="vm.defaultAvailablePackage"]');
     };
 
-    this.getAllSections = function() {
+    this.getAllServices = function() {
         return $$('.mvc-mv');
+    };
+
+    this.getAllServiceHeadingsText = function() {
+        var h3 = $$('h3');
+        h3.each(function(elm){
+            elm.getText().then(function (text) {
+                console.log(text);
+            });
+        });
+        return h3.getText();
+    };
+
+    this.getAllPromotionTexts = function(index) {
+        var css = $$("section:nth-of-type(" + index +  ") span");
+        css.each(function(elm){
+            elm.getText().then(function (text) {
+                console.log(text);
+            });
+        });
+        return css.getText();
     };
 
     this.getAllModals = function() {
@@ -30,8 +60,16 @@ var vvvPage = function (){
         return element(by.css('.i-fvv-messages-detail p')).getText();
     };
 
+    this.getActivarText = function() {
+        return element.all(by.css('.i-fvv-messages-waiting__text')).getText();
+    };
+
     this.getActivarAhoraButton = function() {
         return $('[title="Click para activar paquete"]');
+    };
+
+    this.getDesactivarServiceButton = function() {
+        return $('[title="Click para desactivar paquete"]');
     };
 
     this.getAllContratarButtons = function() {
@@ -39,15 +77,23 @@ var vvvPage = function (){
     };
 
     /*
-    Activar & Contratat modal functions
+    Activar,  Contratat, Desactivar modal functions
      */
 
     this.getActivarButton = function() {
         return $('[value="Activar"]');
     };
 
+    this.getDesactivarButton = function() {
+        return $('[value="Desactivar"]');
+    };
+
     this.getContratarButton = function() {
         return $('[value="Contratar"]');
+    };
+
+    this.getGuardarButton = function() {
+        return $('[value="Guardar"]');
     };
 
     this.getCancelarButton = function() {
@@ -65,6 +111,25 @@ var vvvPage = function (){
     this.getModalClose = function() {
         return $('.i-fvv-modal__btn-close');
         };
+
+    this.getEnterEmail = function() {
+        return $('#email');
+    };
+
+    this.getReEnterEmail = function() {
+        return $('#re-email');
+    };
+
+    this.getAllFormErrorsText = function(index) {
+        var css = ".i-fvv-form-ui__message"
+
+        $$('.i-fvv-form-ui__message').each(function(err){
+            err.getText().then(function (error){
+                console.log(error)
+            });
+        });
+        return $$('.i-fvv-form-ui__message').getText();
+    };
 
 };
 
--- tests/vvv.spec.js
+++ tests/vvv.spec.js
@@ -1,9 +1,8 @@
 describe('testing vvv page', function() {
 
     var main = require('../pages/main.po.js');
-    var vvv = require('../pages/vvv.po.js');
-
-    var rjs = require('../utils/tests.json');
+    var vvv  = require('../pages/vvv.po.js');
+    var rjs  = require('../utils/tests.json');
 
 
     it('test case 1', function() {
@@ -17,24 +16,32 @@ describe('testing vvv page', function() {
         var testUrl = rjs.test1url;
         browser.get(testUrl);
 
-        // nombreUsario
-        expect(vvv.getAllUserLabels().count()).toBe(0);
-        expect(vvv.getAllUserEmails().count()).toBe(0);
+        console.log('\nTest url: ' + testUrl + '\nTest spec: ' + __filename + '\n');
 
-        // activadioServicio = false
-        expect(vvv.getAllOnlineButtons().count()).toBe(0);
+        testNombreUsuarioNull();
 
-        // cable VVV = false
-        // default Z3 does not display
-        expect(vvv.getAllOnlineZ3().count()).toBe(0);
-        expect(vvv.getAllSections().count()).toBe(2);
+        testActivadoServicioFalseOnlineButton();
+
+        testCableVvvvFalseServicioZ3();
 
         // notice text
         var text = rjs.noticeText;
-        expect(vvv.getNoticeText()).toBe(text)
+        expect(vvv.getNoticeText()).toBe(text);
+
+        expect(vvv.getAllServiceHeadingsText()).toContain(rjs.serviceH3text1);
+
+        // promo texts on all services
+        expect(vvv.getAllPromotionTexts(2)).toContain(rjs.amountNoPromo);
+        expect(vvv.getAllPromotionTexts(2)).not.toContain(rjs.gratis);
+
+        expect(vvv.getAllPromotionTexts(3)).toContain(rjs.amountPromo);
+        expect(vvv.getAllPromotionTexts(3)).toContain(rjs.gratis);
+
+        testConratarNombreUsuarioNull(rjs.contratarModal1Heading, 1);
+
+        testConratarNombreUsuarioNull(rjs.contratarModal2Heading, 2);
 
     });
-
 
     it('test case 2', function() {
 
@@ -47,83 +54,209 @@ describe('testing vvv page', function() {
         var testUrl = rjs.test2url;
         browser.get(testUrl);
 
-        // nombreUsario = not null
-        expect(vvv.getAllUserLabels().count()).toBe(1);
-        expect(vvv.getAllUserEmails().count()).toBe(1);
-        expect(vvv.getAllUserEmails().first().getText()).toBe('user@email.com');
+        console.log('\nTest url: ' + testUrl + '\nTest spec: ' + __filename + '\n');
 
-        // activadioServicio = false
-        expect(vvv.getAllOnlineButtons().count()).toBe(0);
+        testNombreUsuarioNotNull();
 
-        // cable VVV = true
-        // default Z3 displays
-        expect(vvv.getAllOnlineZ3().count()).toBe(1);
-        expect(vvv.getAllSections().count()).toBe(4);
+        testActivadoServicioFalseOnlineButton();
+
+        testCableVvvvTrueServicioZ3();
 
         // notice text
         var text = rjs.noticeText;
-        expect(vvv.getNoticeText()).toBe(text)
+        expect(vvv.getNoticeText()).toBe(text);
 
-        /* ServicioZ3 Online Modal
-           Cancelar and Activar button
-         */
-        vvv.getActivarAhoraButton().click()
+        expect(vvv.getAllServiceHeadingsText()).toContain(rjs.serviceH3text1);
+        expect(vvv.getAllServiceHeadingsText()).toContain(rjs.serviceH3text2);
 
-        // modal displays
-        expect(vvv.getAllModals().count()).toBe(1);
+        // promo texts on all services
+        expect(vvv.getAllPromotionTexts(2)).toContain(rjs.amountNoPromo);
+        expect(vvv.getAllPromotionTexts(2)).not.toContain(rjs.gratis);
 
-        // verify header
-        var header = vvv.getModalHeader().getText();
-        expect(header).toBe(rjs.vvvZ3OnlineModalHeader)
+        expect(vvv.getAllPromotionTexts(3)).toContain(rjs.amountPromo);
+        expect(vvv.getAllPromotionTexts(3)).toContain(rjs.gratis);
 
-        // click cancel and modal closes
-        vvv.getCancelarButton().click();
-        expect(vvv.getAllModals().count()).toBe(0);
+        // ServicioZ3 Online Modal
+        testServicioOnlineNombreUsuarioNotNull();
 
-        vvv.getActivarAhoraButton().click()
+        // Paquete Servicioyy2
+        testConratarNombreUsuarioNotNull(rjs.contratarModal1Heading, 1);
 
-        // click on Activar
-        vvv.getActivarButton().click();
-
-        // success displays
-        var header = vvv.getModalHeader().getText();
-        expect(header).toBe(rjs.successModalText)
-
-        // close Modal
-        vvv.getModalClose().click()
-        expect(vvv.getAllModals().count()).toBe(0);
-
-        /* Paquete Servicioyy2
-         Cancelar and Acivar button
-         */
-
-        testConratarEmail(rjs.contratarModal1Header, 1);
-
-        /* Paquete ServicioX1
-         Cancelar and Acivar button
-         */
-        testConratarEmail(rjs.contratarModal2Header, 2);
+        // Paquete ServicioX1
+        testConratarNombreUsuarioNotNull(rjs.contratarModal2Heading, 2);
 
     });
 
+    it('test case 3', function() {
 
+        /*
+         nombreUsario = null
+         activadioServicio = false
+         cable VVV = true
+         */
+
+        var testUrl = rjs.test3url;
+        browser.get(testUrl);
+
+        console.log('\nTest url: ' + testUrl + '\nTest spec: ' + __filename + '\n');
+
+        testNombreUsuarioNull();
+
+        testActivadoServicioFalseOnlineButton();
+
+        testCableVvvvTrueServicioZ3();
+
+        expect(vvv.getAllServiceHeadingsText()).toContain(rjs.serviceH3text1);
+        expect(vvv.getAllServiceHeadingsText()).toContain(rjs.serviceH3text2);
+
+        // promo texts on all services
+        expect(vvv.getAllPromotionTexts(2)).toContain(rjs.amountNoPromo);
+        expect(vvv.getAllPromotionTexts(2)).not.toContain(rjs.gratis);
+
+        expect(vvv.getAllPromotionTexts(3)).toContain(rjs.amountPromo);
+        expect(vvv.getAllPromotionTexts(3)).toContain(rjs.gratis);
+
+        testServicioOnlineNombreUsuarioNull();
+
+        testConratarNombreUsuarioNull(rjs.contratarModal1Heading, 1);
+
+        testConratarNombreUsuarioNull(rjs.contratarModal2Heading, 2);
+
+    });
+
+    it('test case 4', function() {
+
+        /*
+         nombreUsario = not null
+         activadioServicio = true
+         cable VVV = true
+         */
+
+        var testUrl = rjs.test4url;
+        browser.get(testUrl);
+
+        console.log('\nTest url: ' + testUrl + '\nTest spec: ' + __filename + '\n');
+
+        testNombreUsuarioNotNull();
+
+        testActivadoServicioTrueOnlineButton();
+
+        expect(vvv.getAllOnlineButtons().count()).toEqual(2);
+
+        testDesactivar();
+
+        testCableVvvvTrueServicioZ3();
+
+        expect(vvv.getAllServiceHeadingsText()).toContain(rjs.serviceH3text3);
+        expect(vvv.getAllServiceHeadingsText()).toContain(rjs.serviceH3text1);
+        expect(vvv.getAllServiceHeadingsText()).toContain(rjs.serviceH3text4);
+
+        expect(vvv.getAllServices().count()).toEqual(7);
+
+        testServicioOnlineNombreUsuarioNotNull();
+
+        testConratarNombreUsuarioNotNull(rjs.contratarModal1Heading, 1);
+
+        testConratarNombreUsuarioNotNull(rjs.contratarModal2Heading, 2);
+
+        var activateTexts = [rjs.activateText1, rjs.activateText2];
+        expect(vvv.getActivarText()).toEqual(activateTexts);
+
+    });
 
     it('test case 5', function() {
+
+        /*
+         nombreUsario = not null
+         activadioServicio = true
+         cable VVV = false
+         */
 
         var testUrl = rjs.test5url;
         browser.get(testUrl);
 
-        // activadioServicio = true
-        expect(vvv.getAllOnlineButtons().count()).toBe(1);
+        console.log('\nTest url: ' + testUrl + '\nTest spec: ' + __filename + '\n');
 
+        testNombreUsuarioNotNull();
+
+        testActivadoServicioTrueOnlineButton();
+
+        expect(vvv.getAllOnlineButtons().count()).toEqual(1);
+
+        testDesactivar();
+
+        testCableVvvvFalseServicioZ3();
+
+        expect(vvv.getAllServiceHeadingsText()).toContain(rjs.serviceH3text3);
+        expect(vvv.getAllServiceHeadingsText()).toContain(rjs.serviceH3text1);
+        expect(vvv.getAllServiceHeadingsText()).toContain(rjs.serviceH3text4);
+
+        expect(vvv.getAllServices().count()).toEqual(5);
+
+        testConratarNombreUsuarioNotNull(rjs.contratarModal1Heading, 1);
+
+        testConratarNombreUsuarioNotNull(rjs.contratarModal2Heading, 2);
+
+        var activateTexts = [rjs.activateText1, rjs.activateText2];
+        expect(vvv.getActivarText()).toEqual(activateTexts);
     });
+
+
+    /*
+    nombreUsuario
+    panel12 vvv online
+     */
+
+    function testNombreUsuarioNull(){
+        // email and email label do not display
+        expect(vvv.getAllUserLabels().count()).toBe(0);
+        expect(vvv.getAllUserEmails().count()).toBe(0);
+    };
+    
+    function testNombreUsuarioNotNull() {
+        // email and email label display
+        expect(vvv.getAllUserLabels().count()).toBe(1);
+        expect(vvv.getAllUserEmails().count()).toBe(1);
+        expect(vvv.getAllUserEmails().first().getText()).toBe('user@email.com');
+
+        testEditEmail();
+
+    };
+
+    /*
+     panel12 vvv online
+     online button
+     */
+
+    function testActivadoServicioFalseOnlineButton(){
+        // Online button doesnt display
+        expect(vvv.getOnlineButtons().count()).toBe(0);
+    };
+
+    function testActivadoServicioTrueOnlineButton(){
+        // Online button displays
+        expect(vvv.getOnlineButtons().count()).toBe(1);
+    };
+
+    /*
+     ServicioZ3 Online
+     service section
+     */
+
+    function testCableVvvvFalseServicioZ3(){
+        expect(vvv.getAllOnlineZ3().count()).toBe(0);
+    };
+
+    function testCableVvvvTrueServicioZ3(){
+        expect(vvv.getAllOnlineZ3().count()).toBe(1);
+    };
 
 
     /*
     modal functions
      */
 
-    function testConratarEmail(contratarHeader, index) {
+    function testConratarNombreUsuarioNotNull(contratarHeader, index) {
 
         buttons = vvv.getAllContratarButtons();
 
@@ -133,14 +266,14 @@ describe('testing vvv page', function() {
             buttons.last().click();
         }
 
-        // modal displays
+        // displays
         expect(vvv.getAllModals().count()).toBe(1);
 
-        // verify header
+        // header
         var header = vvv.getModalHeader().getText();
         expect(header).toBe(contratarHeader)
 
-        // click cancel, modal closes
+        // close
         vvv.getCancelarButton().click();
         expect(vvv.getAllModals().count()).toBe(0);
 
@@ -150,20 +283,20 @@ describe('testing vvv page', function() {
             buttons.last().click();
         }
 
-        // verify header
+        // header
         expect(header).toBe(contratarHeader)
 
-        // modal displays
+        // displays
         expect(vvv.getAllModals().count()).toBe(1);
 
         vvv.getContratarButton().click();
 
-        // success displays
+        // success
         var success = vvv.getModalHeader().getText();
         expect(success).toBe(rjs.successModalText)
 
-        // close modal
-        vvv.getModalClose().click()
+        // close
+        vvv.getModalClose().click();
         expect(vvv.getAllModals().count()).toBe(0);
 
         // again all the way to success
@@ -175,13 +308,213 @@ describe('testing vvv page', function() {
             buttons.last().click();
         }
 
-        expect(header).toBe(contratarHeader)
+        expect(header).toBe(contratarHeader);
         vvv.getContratarButton().click();
-        expect(success).toBe(rjs.successModalText)
+        expect(success).toBe(rjs.successModalText);
 
-        vvv.getEntendidoButton().click()
+        vvv.getEntendidoButton().click();
         expect(vvv.getAllModals().count()).toBe(0);
 
+    };
+
+    function testConratarNombreUsuarioNull(contratarHeader, index) {
+
+        buttons = vvv.getAllContratarButtons();
+
+        if (index === 1) {
+            buttons.first().click();
+        } else {
+            buttons.last().click();
+        }
+
+        // displays
+        expect(vvv.getAllModals().count()).toBe(1);
+
+        //  header
+        var header = vvv.getModalHeader().getText();
+        expect(header).toBe(contratarHeader);
+
+        //  close
+        vvv.getCancelarButton().click();
+        expect(vvv.getAllModals().count()).toBe(0);
+
+        if (index === 1) {
+            buttons.first().click();
+        } else {
+            buttons.last().click();
+        }
+
+        // header
+        expect(header).toBe(contratarHeader)
+
+        // displays
+        expect(vvv.getAllModals().count()).toBe(1);
+
+        // disabled
+        expect(vvv.getContratarButton().getAttribute('disabled')).toBeTruthy();
+
+        testEmailValidation();
+
+        vvv.getContratarButton().click();
+
+        // success
+        var success = vvv.getModalHeader().getText();
+        expect(success).toBe(rjs.successModalText);
+
+        // close
+        vvv.getEntendidoButton().click();
+        expect(vvv.getAllModals().count()).toBe(0);
+
+    };
+
+    function testServicioOnlineNombreUsuarioNotNull(){
+
+        vvv.getActivarAhoraButton().click();
+
+        // displays
+        expect(vvv.getAllModals().count()).toBe(1);
+
+        // header
+        var header = vvv.getModalHeader().getText();
+        expect(header).toBe(rjs.vvvZ3OnlineModalHeading)
+
+        // close
+        vvv.getCancelarButton().click();
+        expect(vvv.getAllModals().count()).toBe(0);
+
+        vvv.getActivarAhoraButton().click();
+
+        // Activar button
+        vvv.getActivarButton().click();
+
+        // success
+        var header = vvv.getModalHeader().getText();
+        expect(header).toBe(rjs.successModalText);
+
+        // close
+        vvv.getModalClose().click();
+        expect(vvv.getAllModals().count()).toBe(0);
+
+    };
+
+    function testServicioOnlineNombreUsuarioNull(){
+
+        vvv.getActivarAhoraButton().click();
+
+        // modal displays
+        expect(vvv.getAllModals().count()).toBe(1);
+
+        // verify header
+        var header = vvv.getModalHeader().getText();
+        expect(header).toBe(rjs.vvvZ3OnlineModalHeading)
+
+        // click cancel and modal closes
+        vvv.getCancelarButton().click();
+        expect(vvv.getAllModals().count()).toBe(0);
+
+        vvv.getActivarAhoraButton().click();
+
+        // disabled
+        expect(vvv.getActivarButton().getAttribute('disabled')).toBeTruthy();
+
+        testEmailValidation();
+
+        // click on Activar
+        vvv.getActivarButton().click();
+
+        // success displays
+        var header = vvv.getModalHeader().getText();
+        expect(header).toBe(rjs.successModalText)
+
+        // close Modal
+        vvv.getModalClose().click();
+        expect(vvv.getAllModals().count()).toBe(0);
+
+    };
+
+    function testDesactivar(){
+
+        vvv.getDesactivarServiceButton().click();
+
+        // displays
+        expect(vvv.getAllModals().count()).toBe(1);
+
+        // header
+        var header = vvv.getModalHeader().getText();
+        expect(header).toBe(rjs.desactivarModalHeading)
+
+        // close
+        vvv.getCancelarButton().click();
+        expect(vvv.getAllModals().count()).toBe(0);
+
+        vvv.getDesactivarServiceButton().click();
+
+        // Activar button
+        vvv.getDesactivarButton().click();
+
+        // success
+        var header = vvv.getModalHeader().getText();
+        expect(header).toBe(rjs.successModalText);
+
+        // close
+        vvv.getEntendidoButton().click();
+        expect(vvv.getAllModals().count()).toBe(0);
+
+    };
+
+    function testEditEmail(){
+
+        // pencil
+        vvv.getPencilButton().click();
+
+        // header
+        var header = vvv.getModalHeader().getText();
+        expect(header).toBe(rjs.cambiaModalHeading)
+
+        // disabled
+        expect(vvv.getGuardarButton().getAttribute('disabled')).toBeTruthy();
+
+        testEmailValidation();
+
+        vvv.getGuardarButton().click();
+
+        var success = vvv.getModalHeader().getText();
+        expect(success).toBe(rjs.successModalText)
+
+        // close
+        vvv.getEntendidoButton().click();
+        expect(vvv.getAllModals().count()).toBe(0);
+
+        // pencil
+        vvv.getPencilButton().click();
+
+        // close
+        vvv.getCancelarButton().click();
+        expect(vvv.getAllModals().count()).toBe(0);
+
+    };
+
+    function testEmailValidation(){
+
+        var OkEmail = 'format@test.com'
+        var BadEmail = 'format@'
+
+        vvv.getEnterEmail().sendKeys(BadEmail);
+
+        expect(vvv.getAllFormErrorsText()).toContain(rjs.invalidEmail);
+
+        vvv.getEnterEmail().clear();
+
+        vvv.getEnterEmail().sendKeys(OkEmail);
+
+        vvv.getReEnterEmail().sendKeys(BadEmail);
+
+        expect(vvv.getAllFormErrorsText()).toContain(rjs.dontMatchEmail);
+        expect(vvv.getAllFormErrorsText()).not.toContain(rjs.invalidEmail);
+
+        vvv.getReEnterEmail().clear();
+
+        vvv.getReEnterEmail().sendKeys(OkEmail);
     };
 
 });
